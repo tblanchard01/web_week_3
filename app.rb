b72c0ea22e646d360120b0e9cacbf61a9b687111
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-07-25 10:01:58 +0100

    added erb page

--- app.rb
+++ app.rb
@@ -1,11 +1,13 @@
 require 'sinatra'
 get '/' do
-    "Hello World"
+    "hello friend"
   end
   get '/secret' do
     'This is a secret page'
   end
 
   get '/cat' do 
-  
+    erb(:index)
 end 
+
+#https://github.com/makersacademy/course/blob/master/intro_to_the_web/sinatra_returning_html.md
